3dbb1f85967b9fa8b53ce22f81fa5e891f43d14c
mo-arman <moharman3818@gmail.com>
2024-07-05 14:34:44 +0530

    Secand Lect Learn and Practice Ignite our App

--- App.js
+++ App.js
@@ -11,12 +11,12 @@
 
   const parent = React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},[
-        React.createElement("h2",{},"I am H1 Tag and My Father Name Div"),
-        React.createElement("h2",{},"I am H2 Tag and My Father Name Also div because H1 and H2 Siblings")
+        React.createElement("h2",{},"I am H1 Tag and My Father Name Div🧒🏻"),
+        React.createElement("h2",{},"I am H2 Tag and My Father Name Also div because H1 and H2 Siblings🧒🏻")
     ]),
     React.createElement("div",{id:"child"},[
-        React.createElement("h2",{},"I am H3 Tag and My Father Name Div"),
-        React.createElement("h2",{},"I am H4 Tag and My Father Name Also div because H1 and H2 Siblings")
+        React.createElement("h2",{},"I am H3 Tag and My Father Name Div🧒🏻"),
+        React.createElement("h2",{},"I am H4 Tag and My Father Name Also div because H1 and H2 Siblings🧒🏻")
     ])
   );
  console.log(parent);
